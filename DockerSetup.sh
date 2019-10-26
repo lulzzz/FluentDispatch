d423c3a907449feffb9ad71b5dcd7d583dc502ea
Ben <bbougot@users.noreply.github.com>
2019-10-26 13:15:01 +0200

    Fix Unix script

--- DockerSetup.sh
+++ DockerSetup.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
+docker network create --driver=bridge --subnet=192.168.2.0/24 --gateway=192.168.2.10 gcd
+docker volume create --name=grafana-volume
+docker volume create --name=influxdb-volume
+
 cd GrandCentralDispatch.Node/
 docker-compose down --rmi all
 docker-compose up -d --force-recreate --build
 
 cd ../GrandCentralDispatch.Cluster/
 docker-compose down --rmi all
-docker network create --driver=bridge --subnet=192.168.2.0/24 --gateway=192.168.2.10 gcd
-docker volume create --name=grafana-volume
-docker volume create --name=influxdb-volume
 docker-compose up -d --force-recreate --build
