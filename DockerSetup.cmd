129ea5244152f372207d63b130ce6bf5bf3a0676
Ben <bbougot@users.noreply.github.com>
2019-11-01 21:42:00 +0100

    Fix issues

--- DockerSetup.cmd
+++ DockerSetup.cmd
@@ -4,20 +4,20 @@
 
 TITLE GrandCentralDispatch Docker Setup
 
-ECHO Please wait... Initializing Docker Swarm.
-docker swarm init
-
 ECHO Please wait... Creating Docker network bridge.
-docker network create --driver=bridge --subnet=192.168.2.0/24 --gateway=192.168.2.10 gcd
+docker network create --driver=bridge --subnet=192.168.2.0/24 --gateway=192.168.2.10 gcd-network
 
 ECHO Please wait... Creating Docker volumes.
 docker volume create --name=grafana-volume
 docker volume create --name=influxdb-volume
+docker volume create --name=elasticsearch-volume
 
+ECHO Please wait... Deploying GCD Node.
 cd GrandCentralDispatch.Node/
 docker-compose down --rmi all
 docker-compose up -d --force-recreate --build
 
+ECHO Please wait... Deploying GCD Cluster.
 cd ../GrandCentralDispatch.Cluster/
 docker-compose down --rmi all
 docker-compose up -d --force-recreate --build
--- DockerSetup.sh
+++ DockerSetup.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
-docker network create --driver=bridge --subnet=192.168.2.0/24 --gateway=192.168.2.10 gcd
+echo "Please wait... Creating Docker network bridge."
+docker network create --driver=bridge --subnet=192.168.2.0/24 --gateway=192.168.2.10 gcd-network
+
+echo "Please wait... Creating Docker volumes."
 docker volume create --name=grafana-volume
 docker volume create --name=influxdb-volume
+docker volume create --name=elasticsearch-volume
 
+echo "Please wait... Deploying GCD Node."
 cd GrandCentralDispatch.Node/
 docker-compose down --rmi all
 docker-compose up -d --force-recreate --build
 
+echo "Please wait... Deploying GCD Cluster."
 cd ../GrandCentralDispatch.Cluster/
 docker-compose down --rmi all
 docker-compose up -d --force-recreate --build
